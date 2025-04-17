d73b782af7827b6f482610755febddae42f0b162
axtk <axtkx@inbox.ru>
2025-04-17 21:04:10 +0300

    add tsdoc to config type

--- src/types/BinConfig.ts
+++ src/types/BinConfig.ts
@@ -1,9 +1,19 @@
 import type {ContentConfig} from './ContentConfig';
 
 export type BinConfig = Omit<ContentConfig, 'scriptSrc'> & {
+    /** GitHub Pages branch */
     ghPagesBranch?: string;
     mainBranch?: string;
+    /** Whether to remove the GitHub Pages branch and quit */
     remove?: boolean;
+    /** Content of the './CNAME' file */
     cname?: string;
+    /**
+     * As a boolean, it means whether to add the
+     * '<package_name>.js.org' domain to the './CNAME' file.
+     *
+     * As a string, it sets the '<jsorg_value>.js.org' domain
+     * to the './CNAME' file.
+     */
     jsorg?: boolean | string;
 };
