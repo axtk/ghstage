41e0b7ee236bfea6f330485d3d8b9ba24a7e91ae
axtk <axtkx@inbox.ru>
2025-04-21 13:04:42 +0300

    add support for lighter base colors in favicon

--- src/utils/getIcon.ts
+++ src/utils/getIcon.ts
@@ -1,11 +1,11 @@
 export function getIcon(baseColor = 'gray') {
     return `
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100">
-        <style>.b{fill:${baseColor};}.c0{fill:rgba(255,255,255,.6);}.c1{fill:rgba(255,255,255,.3);}.c2{fill:rgba(255,255,255,0);}</style>
+        <style>g{--b:${baseColor};--c:oklch(from var(--b) calc(100*(.78 - l)) 0 0);}.b{fill:var(--b);}.c0{fill:color(from var(--c) srgb r g b / .5);}.c1{fill:color(from var(--c) srgb r g b / .3);}.c2{fill:none;}</style>
         <g stroke="none">
             <path d="M0,15 L50,0 L100,15 L100,78 L50,100 L0,81z" stroke="none" class="b"/>
-            <path d="M0,15 L50,30 L100,15 L50,0z" stroke="none" class="c0"/>
-            <path d="M0,15 L50,30 L50,100 L0,81z" stroke="none" class="c1"/>
+            <path d="M0,15 L50,30 L100,15 L50,0z" stroke="none" class="c1"/>
+            <path d="M0,15 L50,30 L50,100 L0,81z" stroke="none" class="c0"/>
             <path d="M50,30 L100,15 L100,78 L50,100z" stroke="none" class="c2"/>
         </g>
         </svg>
