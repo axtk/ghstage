5a3732b38910f4f093986b6fd697bc97c48f076a
axtk <axtkx@inbox.ru>
2025-05-27 15:31:05 +0300

    lint

--- src/content/layout/withPageNav.ts
+++ src/content/layout/withPageNav.ts
@@ -37,7 +37,8 @@ export function withPageNav(sections: Element[]) {
                 : null,
             next
                 ? `<span class="next">${next} <span class="icon">&rarr;</span></span>`
-                : repoLink && `<span class="next repo">${repoLink} <span class="icon">✦</span></span>`,
+                : repoLink &&
+                  `<span class="next repo">${repoLink} <span class="icon">✦</span></span>`,
         ]
             .filter(item => item !== null)
             .join('<span class="sep"> | </span>');
