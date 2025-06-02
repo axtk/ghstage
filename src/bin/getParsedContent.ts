26affd77cf1fdce1f2a7943cdfe796a5d01e38a1
axtk <axtkx@inbox.ru>
2025-06-02 19:07:48 +0300

    fix installation

--- src/bin/getParsedContent.ts
+++ src/bin/getParsedContent.ts
@@ -101,9 +101,9 @@ export async function getParsedContent() {
 
             featuresComplete = true;
 
-            let installationMatches = line.match(/`npm (i|install) [^`]+`/);
+            let installationMatches = line.match(/`(npm (i|install) [^`]+)`/);
 
-            if (installationMatches) installation = installationMatches[0];
+            if (installationMatches) installation = installationMatches[1];
             else note.push(line);
         }
     }
