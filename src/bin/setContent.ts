2c064fd72aed84f93f7fdabf8b6946f5d81b78b2
axtk <axtkx@inbox.ru>
2025-06-02 20:15:32 +0300

    remove leading spaces in files

--- src/bin/setContent.ts
+++ src/bin/setContent.ts
@@ -52,154 +52,154 @@ export async function setContent() {
             writeFile(
                 `./${contentDir}/${nav[i].id ?? `_untitled_${i}`}.md`,
                 toFileContent(`
-            ---
-            layout: section
-            section:
-                id: "${nav[i].id ?? ''}"
-                title: "${md.renderInline(nav[i].title ?? '')}"
-            prev_section:
-                id: "${nav[i - 1]?.id ?? ''}"
-                title: "${md.renderInline(nav[i - 1]?.title ?? '')}"
-            next_section:
-                id: "${nav[i + 1]?.id ?? ''}"
-                title: "${md.renderInline(nav[i + 1]?.title ?? '')}"
-            ---
+---
+layout: section
+section:
+    id: "${nav[i].id ?? ''}"
+    title: "${md.renderInline(nav[i].title ?? '')}"
+prev_section:
+    id: "${nav[i - 1]?.id ?? ''}"
+    title: "${md.renderInline(nav[i - 1]?.title ?? '')}"
+next_section:
+    id: "${nav[i + 1]?.id ?? ''}"
+    title: "${md.renderInline(nav[i + 1]?.title ?? '')}"
+---
 
-            ${content}
+${content}
             `),
             ),
         ),
         writeFile(
             './_layouts/index.html',
             toFileContent(`
-            <!DOCTYPE html>
-            <html lang="en"${colorScheme ? ` style="--color-scheme: ${colorScheme}"` : ''}>
-            <head>
-            <meta charset="utf-8">
-            <meta name="viewport" content="width=device-width, initial-scale=1">
-            <title>${escapedName}${packageDescription ? ` | ${escapeHTML(packageDescription)}` : ''}</title>
-            <link rel="stylesheet" href="${packageUrl}/dist/css/base.css">
-            <link rel="stylesheet" href="${packageUrl}/dist/css/index.css">
-            <link rel="stylesheet" href="${packageUrl}/dist/css/code.lightbulb.css">
-            <link rel="icon" type="image/svg+xml" href="{{site.github.baseurl}}/favicon.svg">
-            </head>
-            <body>
-            <div class="layout">
-            <main>
-            {{content}}
-            </main>
-            </div>
+<!DOCTYPE html>
+<html lang="en"${colorScheme ? ` style="--color-scheme: ${colorScheme}"` : ''}>
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>${escapedName}${packageDescription ? ` | ${escapeHTML(packageDescription)}` : ''}</title>
+<link rel="stylesheet" href="${packageUrl}/dist/css/base.css">
+<link rel="stylesheet" href="${packageUrl}/dist/css/index.css">
+<link rel="stylesheet" href="${packageUrl}/dist/css/code.lightbulb.css">
+<link rel="icon" type="image/svg+xml" href="{{site.github.baseurl}}/favicon.svg">
+</head>
+<body>
+<div class="layout">
+<main>
+{{content}}
+</main>
+</div>
 
-            ${counterContent}
-            </body>
-            </html>
+${counterContent}
+</body>
+</html>
             `),
         ),
         writeFile(
             './index.html',
             toFileContent(`
-            ---
-            layout: index
-            ---
+---
+layout: index
+---
 
-            <section class="intro-title">
-                <div class="badges">
-                    ${md.render(badges)}
-                </div>
-                <h1>${name}</h1>
-                <div class="description">
-                    ${md.render(description)}
-                </div>
-                <p class="actions">
-                    <a href="{{site.github.baseurl}}/start" class="primary button">Docs ›››</a>
-                    <span class="sep"> • </span>
-                    ${await getRepoLink('button')}
-                </p>
-            </section>
-            <section class="intro">
-                <div class="features">
-                    ${md.render(features)}
-                </div>
-                <p class="installation"><code>${escapeHTML(installation)}</code></p>
-            </section>
+<section class="intro-title">
+    <div class="badges">
+        ${md.render(badges)}
+    </div>
+    <h1>${name}</h1>
+    <div class="description">
+        ${md.render(description)}
+    </div>
+    <p class="actions">
+        <a href="{{site.github.baseurl}}/start" class="primary button">Docs ›››</a>
+        <span class="sep"> • </span>
+        ${await getRepoLink('button')}
+    </p>
+</section>
+<section class="intro">
+    <div class="features">
+        ${md.render(features)}
+    </div>
+    <p class="installation"><code>${escapeHTML(installation)}</code></p>
+</section>
             `),
         ),
         writeFile(
             './_layouts/section.html',
             toFileContent(`
-            <!DOCTYPE html>
-            <html lang="en"${colorScheme ? ` style="--color-scheme: ${colorScheme}"` : ''}>
-            <head>
-            <meta charset="utf-8">
-            <meta name="viewport" content="width=device-width, initial-scale=1">
-            <title>{{page.section.title | strip_html}} | ${escapedName}</title>
-            <link rel="stylesheet" href="${packageUrl}/dist/css/base.css">
-            <link rel="stylesheet" href="${packageUrl}/dist/css/section.css">
-            <link rel="stylesheet" href="${packageUrl}/dist/css/code.lightbulb.css">
-            <link rel="icon" type="image/svg+xml" href="{{site.github.baseurl}}/favicon.svg">
-            </head>
-            <body>
-            <div class="layout">
-            <div class="${navContent ? '' : 'no-nav '}body">
-            ${navContent}
-            <main>
-            {{content}}
+<!DOCTYPE html>
+<html lang="en"${colorScheme ? ` style="--color-scheme: ${colorScheme}"` : ''}>
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>{{page.section.title | strip_html}} | ${escapedName}</title>
+<link rel="stylesheet" href="${packageUrl}/dist/css/base.css">
+<link rel="stylesheet" href="${packageUrl}/dist/css/section.css">
+<link rel="stylesheet" href="${packageUrl}/dist/css/code.lightbulb.css">
+<link rel="icon" type="image/svg+xml" href="{{site.github.baseurl}}/favicon.svg">
+</head>
+<body>
+<div class="layout">
+<div class="${navContent ? '' : 'no-nav '}body">
+${navContent}
+<main>
+{{content}}
 
-            <p class="pagenav">
-                <span class="prev">
-                    <span class="icon">←</span>
-                    {% if page.prev_section.id == "" %}<a href="{{site.github.baseurl}}/">Intro</a>{% else %}<a href="{{site.github.baseurl}}/${contentDir}/{{page.prev_section.id}}">{{page.prev_section.title}}</a>{% endif %}
-                </span>
-                <span class="sep">|</span>
-                {% if page.next_section.id == "" %}
-                <span class="repo next">
-                    ${await getRepoLink()}
-                    <span class="icon">✦</span>
-                </span>
-                {% else %}
-                <span class="next">
-                    <a href="{{site.github.baseurl}}/${contentDir}/{{page.next_section.id}}">{{page.next_section.title}}</a>
-                    <span class="icon">→</span>
-                </span>
-                {% endif %}
-            </p>
-            </main>
-            </div>
-            </div>
+<p class="pagenav">
+    <span class="prev">
+        <span class="icon">←</span>
+        {% if page.prev_section.id == "" %}<a href="{{site.github.baseurl}}/">Intro</a>{% else %}<a href="{{site.github.baseurl}}/${contentDir}/{{page.prev_section.id}}">{{page.prev_section.title}}</a>{% endif %}
+    </span>
+    <span class="sep">|</span>
+    {% if page.next_section.id == "" %}
+    <span class="repo next">
+        ${await getRepoLink()}
+        <span class="icon">✦</span>
+    </span>
+    {% else %}
+    <span class="next">
+        <a href="{{site.github.baseurl}}/${contentDir}/{{page.next_section.id}}">{{page.next_section.title}}</a>
+        <span class="icon">→</span>
+    </span>
+    {% endif %}
+</p>
+</main>
+</div>
+</div>
 
-            ${counterContent}
-            </body>
-            </html>
+${counterContent}
+</body>
+</html>
             `),
         ),
         writeFile(
             './_layouts/start.html',
             toFileContent(`
-            <!DOCTYPE html>
-            <html lang="en" class="blank"${colorScheme ? ` style="--color-scheme: ${colorScheme}"` : ''}>
-            <head>
-                <meta charset="utf-8">
-                <meta name="viewport" content="width=device-width">
-                <meta http-equiv="refresh" content="0; URL={{site.github.baseurl}}/${contentDir}/{{page.start_section_id}}">
-                <title>${escapedName}</title>
-                <link rel="stylesheet" href="${packageUrl}/dist/css/base.css">
-                <link rel="icon" type="image/svg+xml" href="{{site.github.baseurl}}/favicon.svg">
-            </head>
-            <body>
-            <div class="layout">
-                <h1>${escapedName}</h1>
-            </div>
-            </body>
-            </html>
+<!DOCTYPE html>
+<html lang="en" class="blank"${colorScheme ? ` style="--color-scheme: ${colorScheme}"` : ''}>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width">
+    <meta http-equiv="refresh" content="0; URL={{site.github.baseurl}}/${contentDir}/{{page.start_section_id}}">
+    <title>${escapedName}</title>
+    <link rel="stylesheet" href="${packageUrl}/dist/css/base.css">
+    <link rel="icon" type="image/svg+xml" href="{{site.github.baseurl}}/favicon.svg">
+</head>
+<body>
+<div class="layout">
+    <h1>${escapedName}</h1>
+</div>
+</body>
+</html>
             `),
         ),
         writeFile(
             './start.html',
             toFileContent(`
-            ---
-            layout: start
-            start_section_id: "${nav[0].id}"
-            ---
+---
+layout: start
+start_section_id: "${nav[0].id}"
+---
             `),
         ),
     ]);
--- src/bin/toFileContent.ts
+++ src/bin/toFileContent.ts
@@ -1,13 +1,3 @@
 export function toFileContent(x: string) {
-    let lines = x.split(/\r?\n/);
-    let firstLine = lines.find(x => x.trim() !== '') ?? '';
-    let tab = firstLine.match(/^\s+/)?.[0];
-
-    if (!tab) return `${x.trim()}\n`;
-
-    for (let i = 0; i < lines.length; i++) {
-        if (lines[i].startsWith(tab)) lines[i] = lines[i].slice(tab.length);
-    }
-
-    return `${lines.join('\n').trim()}\n`;
+    return `${x.trim()}\n`;
 }
