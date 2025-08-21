a8fad65aaa2407b526bb4348dcebde7941529696
axtk <axtkx@inbox.ru>
2025-08-21 18:24:38 +0300

    update ym code

--- src/bin/getCounterContent.ts
+++ src/bin/getCounterContent.ts
@@ -11,9 +11,9 @@ export async function getCounterContent() {
 m[i].l=1*new Date();
 for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
 k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+(window, document, "script", "https://mc.yandex.com/metrika/tag.js", "ym");
 ym(${ymid}, "init", {clickmap: true, trackLinks: true, accurateTrackBounce: true});
 </script>
-<noscript><div><img src="https://mc.yandex.ru/watch/${ymid}" style="position:absolute;left:-9999px;" alt=""></div></noscript>
+<noscript><div><img src="https://mc.yandex.com/watch/${ymid}" style="position:absolute;left:-9999px;" alt=""></div></noscript>
         `.trim();
 }
