8ff836ffa6086fabf5cc6aafbca001238de4703f
axtk <axtkx@inbox.ru>
2025-09-22 13:29:50 +0300

    lint

--- src/utils/escapeHTML.ts
+++ src/utils/escapeHTML.ts
@@ -8,8 +8,7 @@ const htmlEntityMap: [string, string][] = [
 export function escapeHTML(x: unknown): string {
     let s = String(x);
 
-    if (!x)
-        return '';
+    if (!x) return '';
 
     for (let [character, htmlEntity] of htmlEntityMap)
         s = s.replaceAll(character, htmlEntity);
